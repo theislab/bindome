772a9966e40c4e74864bc15517a66fa65ed72a57
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2022-05-16 11:20:04 +0200

    adding functionality for parsing coordinates

--- bindome/__init__.py
+++ bindome/__init__.py
@@ -1,3 +1,4 @@
 import bindome.datasets
 import bindome.constants
+import bindome.tl
 from bindome.constants import set_anndata_directory
---
+++ bindome/tl/__init__.py
@@ -0,0 +1,1 @@
+from bindome.tl.fasta import get_sequences_from_bed
