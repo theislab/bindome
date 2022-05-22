4a13db8daaef5424ee2b542a7697e87a6bd95c57
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2022-05-22 20:04:17 +0200

    adding multibatch loading function (bindome)

--- bindome/datasets/probound.py
+++ bindome/datasets/probound.py
@@ -11,3 +11,15 @@ class ProBound():
                         sep='\t', header=None)
         df.columns = ['seq', 0, 1]
         return df
+
+    @staticmethod
+    def GR_mult_conc():
+        probound_dir = bd.constants.ANNOTATIONS_DIRECTORY + '/chipseq/probound'
+        df2 = []
+        for i, conc in enumerate(['30', '300', '3000']):
+            df = pd.read_csv(os.path.join(probound_dir, 'countTable.%i.GR_%s.tsv.gz' % (i, conc)),
+                            sep='\t', header=None)
+            df.columns = ['seq', 0, 1]
+            df['batch'] = conc
+            df2.append(df)
+        return pd.concat(df2)
